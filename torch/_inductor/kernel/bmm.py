6c61701842c3edba516bdeedb85ae27845ede3a2
Ruben Rodriguez Buchillon <coconutruben@fb.com>
2025-08-27 00:16:21 -0700

    [inductor][choices] pass through layout and input_nodes
    \# why
    
    - params already available in get_mm_configs
    - simplifies the code
    - adds a possibility to edit the nodes/layout in
      a centralized place
    
    \# what
    
    - add layout and input_nodes into extra_kwargs
    - no other modifications
    
    \# testing
    
    ```
    python3 -bb -m pytest test/inductor/test_max_autotune.py -v
    ```
    
    ghstack-source-id: b3b9b5e108f04919562bc0a382e34fef17c3099a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/161338

--- torch/_inductor/kernel/bmm.py
+++ torch/_inductor/kernel/bmm.py
@@ -209,8 +209,6 @@ def tuned_bmm(mat1, mat2, out_dtype=None, *, layout=None):
         ):
             bmm_template.maybe_append_choice(
                 choices,
-                input_nodes=kernel_inputs.nodes(),
-                layout=layout,
                 **kwargs,
                 **extra_kwargs,
             )
@@ -282,8 +280,6 @@ def tuned_baddbmm(inp, mat1, mat2, *, alpha=1, beta=1, layout=None):
         ):
             bmm_template.maybe_append_choice(
                 choices,
-                input_nodes=kernel_inputs.nodes(),
-                layout=layout,
                 **kwargs,
                 **extra_kwargs,
             )
